ef8d77eaeec4d9949ce8d345fec943f532dcc2f1
Danilo Maccari <maccari78@gmail.com>
2021-11-09 17:22:11 -0300

    take notes and last fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'css/styles'
 
